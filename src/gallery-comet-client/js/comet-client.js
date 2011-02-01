2c54eaea65dec61c58d05f7dc088e208b4738aa3
Morgan 2 <morgan.chengmo@gmail.com>
2011-02-01 10:45:12 +0800

    Add support of HTTP Streaming to IE

--- src/gallery-comet-client/js/comet-client.js
+++ src/gallery-comet-client/js/comet-client.js
@@ -24,13 +24,37 @@ CometClient.prototype = {
          */
         this._lastPushedIdx = 0;
 
-        xhr = this._createXHR();
-        
-        xhr.onreadystatechange = function() {
-            that._onXhrStateChange(xhr);
-        };
-        xhr.open('GET', this.url, true);
-        xhr.send();
+        if (Y.UA.ie) {
+            this._createIFrame(this.url, function(data) { 
+                if (that.cfg && that.cfg.on && that.cfg.on.pushed) {
+                    //TODO: it is possible that multiple messages are pushed together. There should be a way to delimit messages.
+                    //
+                    that.cfg.on.pushed(data);
+                }
+            });
+
+        } else {
+            xhr = this._createXHR();
+            
+            xhr.onreadystatechange = function() {
+                that._onXhrStateChange(xhr);
+            };
+            xhr.open('GET', this.url, true);
+            xhr.send();
+
+            /*
+            Y.later(100, this, function() {
+                this.fire('comet:pushed', xhr.responseText);
+
+                if (this.cfg && this.cfg.on && this.cfg.on.pushed) {
+                    //TODO: it is possible that multiple messages are pushed together. There should be a way to delimit messages.
+                    //
+                    this.cfg.on.pushed(xhr.responseText);
+                }
+
+            }, null, true);
+            */
+        }
     },
 
     _onXhrStateChange: function(xhr) {
@@ -56,6 +80,28 @@ CometClient.prototype = {
         }
     },
 
+    _createIFrame: function(url, callback) {
+        transDoc = new ActiveXObject('htmlfile');
+        transDoc.open();
+        //TODO: perhaps use diferent domain
+        //transDoc.write('<html><script type="text/javascript">document.domain="' + document.domain + '";</script></html>');
+        transDoc.write('<html></html>');
+        transDoc.close();
+
+        transDoc.parentWindow.callback = callback;
+        transDoc.parentWindow.mo = 'mo';
+
+        var iframeDiv = transDoc.createElement('div');
+        transDoc.body.appendChild(iframeDiv);
+        iframeDiv.innerHTML = '<iframe src="' + url + '"></iframe>';
+
+        setTimeout(function() {
+            //this.dummy = transDoc.parentWindow;
+            //transDoc.parentWindow.callback('hello');
+        }, 2000);
+
+    },
+
     /**
      * create an XMLHttpRequest according to current browser.
      *
@@ -72,7 +118,7 @@ CometClient.prototype = {
             // IE5/6 falls into this block
             //
             // NOTE: Actually, we cannot implement comet with IE XHR since it doesn't trigger readyState===3 state in progress.
-            // We keep below code for Long-Poll.
+            // I keep below code for Long-Poll(perhaps in the future)
             try {
                 xhrObj = new window.ActiveXObject('Msxml2.XMLHTTP');
             } catch (e1) {
