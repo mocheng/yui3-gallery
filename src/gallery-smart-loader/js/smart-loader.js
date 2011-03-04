5fa49b95a7d2d9c6eca2f9f73882fb2f462d0519
Morgan 2 <morgan.chengmo@gmail.com>
2011-03-04 18:29:15 +0800

    start up

--- src/gallery-smart-loader/js/smart-loader.js
+++ src/gallery-smart-loader/js/smart-loader.js
@@ -1,2 +1,30 @@
-Y.suse = function() {
+function getLoader() {
+    // loader should have been loaded
+    return Y.Env._loader;
+}
+
+Y.smartUse = function() {
+    var args = Array.prototype.slice.call(arguments, 0),
+        callback = args[args.length - 1];
+
+    if (Y.Lang.isFunction(callback)) {
+        args.pop();
+    } else {
+        callback = null;
+    }
+};
+
+Y.getDependent = function(modules) {
+    var loader = getLoader(),
+        dependents;
+
+    console.log(modules);
+
+    loader.require(modules);
+    loader.ignoreRegistered = true;
+    loader.calculate(null, 'js');
+    dependents = loader.sorted;
+
+    console.log(dependents);
+    return dependents;
 };
